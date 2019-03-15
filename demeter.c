22a55b3fe3dbe4780f977419ccdb87efdeca6e81
Brandon Lucas <thebrandonlucas@gmail.com>
2019-03-15 11:36:16 -0500

    finished data load, adding curses

--- demeter.c
+++ demeter.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ncurses.h>
+#include <unistd.h>
 
 #define BUFFER_SIZE 1024
-#define PRODUCTS_SIZE 239090
+#define PRODUCTS_SIZE 228805
 #define DERIVATION_CODE_DESCRIPTION_SIZE 9
 #define SERVING_SIZE 237911
 #define NUTRIENTS_SIZE 1048576
 
 
-struct Products {
+struct Product {
 	int NBD_Number; 
 	char* long_name; 
 	// TODO: change back to char [2]?
@@ -21,64 +23,63 @@ struct Products {
 	char* ingredients_english; 
 };
 
-// TODO: create a generalized function for reading CSV data of any parameters. (parseCSV)
-
-//void parseRowCSV(char* filename, struct products* productList, int FILE_SIZE) {
-//
-//}
-
-void loadProductData(char* filename, int fileSize, struct Products productList[]) {
-//void loadFileData(char* filename, char* whichFile) {
-	FILE* productInput;
+void loadProductData(char* filename, int fileSize, struct Product *productList[]) {
+	FILE* productFile;
 	char buffer[BUFFER_SIZE];
-	productInput = fopen(filename, "r");
-	//struct products productList[PRODUCTS_SIZE];
-	 //filter out columns
-	fgets(buffer, sizeof(buffer), productInput);
+	productFile = fopen(filename, "r");
+	//fgets(buffer, sizeof(buffer), productFile);
 	char* token;
-	struct Products product = { .NBD_Number = 0, .long_name = "", .data_source = "", .gtin_upc = 0, 
+	struct Product product = { .NBD_Number = 0, .long_name = "", .data_source = "", .gtin_upc = 0, 
 								 .manufacturer = "", .date_modified = "", .date_available = "", 
 								 .ingredients_english = ""};
 
 	// place each line (struct) in array
 	for (int i = 0; i < fileSize; i++) {
-		// TODO: initialize to eliminate compiler warnings
-		fgets(buffer, sizeof(buffer), productInput);
+		fgets(buffer, sizeof(buffer), productFile);
 		token = strtok(buffer, "~"); 
-		// FIXME: this is the part you'll replace to refactor it
-		product.NBD_Number = atoi(token); token = strtok(NULL, "~");
-		product.long_name = token; token = strtok(NULL, "~");
-		product.data_source = token; token = strtok(NULL, "~");
+		product.NBD_Number = atoi(token); token = strtok(NULL, "~"); 
+		product.long_name = token; token = strtok(NULL, "~"); 
+		product.data_source = token; token = strtok(NULL, "~");  
 		product.gtin_upc = atoi(token); token = strtok(NULL, "~");
 		product.manufacturer = token; token = strtok(NULL, "~");
 		product.date_modified = token; token = strtok(NULL, "~");
 		product.date_available = token; token = strtok(NULL, "~");
 		product.ingredients_english = token;
-		productList[i] = product; 
-		printf("%s", productList[i].long_name); 
-		printf("hello"); 
+		productList[i] = &product; 
 	}
-	fclose(productInput); 
+	fclose(productFile);
 	//printf("%s\n%s\n", productList[0].long_name, productList[PRODUCTS_SIZE - 1].long_name);
 	return; 
 }
 
 void printMenu() {
+	//WINDOW *w; 
+	//char list[5][7] = { "One", "Two", "Three", "Four", "Five" }; 
+	//char item[7]; 
+	//int ch, i = 0, width = 7; 
+
+	//initscr(); // Initialize window
+	//w = newwin(10, 12, 1, 1); 
+	//box(w, 0, 0); 
+	//noecho(); // Don't echo any keypresses
+	//curs_set(FALSE); // Don't display a cursor
 	printf("1 - List Your Summary\n"); 
 	printf("2 - Add Meal\n"); 
 	printf("3 - Update Meal\n"); 
 	printf("4 - Delete Meal\n"); 
 	printf("5 - Exit\n\n"); 
+	
+	//sleep(1); 
 	return; 
 }
 
 int main() {
-	char* productFile = "C:\\Users\\thebr\\Downloads\\BFPD_csv_07132018\\Products.csv"; 
-	struct Products productList;
+	char* productFile = "food_database.csv";
+	struct Product* productList[PRODUCTS_SIZE];
 	//loadProductData(productFile, productList);
-	loadProductData(productFile, PRODUCTS_SIZE, &productList); 
+	loadProductData(productFile, PRODUCTS_SIZE, productList); 
 	printMenu(); 
-	printf("Choose an option: "); 
+	//printf("Choose an option: "); 
 	//int menuChoice; 
 	//scanf("%d", &menuChoice); 
 
@@ -87,5 +88,8 @@ int main() {
 	//switch (menuChoice) {
 
 	//}
+
+	//endwin(); // Restore normal terminal behavior
+
 	return 0; 
 }
